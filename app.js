7c8ac6d4e81580c4b06e11b303b92b52640a2924
SSubha16 <ssubha3333@gmail.com>
2024-08-24 01:20:29 +0530

    add project files

--- app.js
+++ app.js
@@ -10,6 +10,7 @@ const methodOverride = require("method-override");
 const ejsMAte = require("ejs-mate");
 const ExpressError = require("./utils/ExpressError.js");
 const session = require("express-session");
+const MongoStore = require('connect-mongo');
 const flash = require("connect-flash");
 const passport = require("passport");
 const LocalStrategy = require("passport-local");
@@ -22,7 +23,6 @@ const reviewRouter = require("./routes/review.js");
 const userRouter = require("./routes/user.js");
 
 
-// const MONGO_URL = "mongodb://127.0.0.1:27017/wanderlust";
 
 const dbUrl = process.env.ATLASDB_URL;
 
@@ -45,8 +45,21 @@ app.use(express.static(path.join(__dirname , "/public")));
 app.use(express.json());
 
 
+const store = MongoStore.create({
+    mongoUrl : dbUrl ,
+    crypto : {
+        secret : process.env.SECRET,
+    },
+    touchAfter : 24 * 3600 ,
+});
+
+store.on("err" , () => {
+    console.log("ERROR IN MONGO SESSION STORE" , err);
+});
+
 const sessionOptions = {
-    secret : "mysupersecret",
+    store,
+    secret : process.env.SECRET,
     resave : false ,
     saveUninitialized : true ,
     cookie : {
@@ -55,6 +68,7 @@ const sessionOptions = {
        httpOnly : true,
     },
 };
+
 
 // app.get("/" , (req, res) => {
 //     res.send("hi ,i am root");
